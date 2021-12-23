adcea87f621a9fe48f5f6d9e719f81b2afdf9051
amir maleki <amirmaleki21physics@gmail.com>
2021-12-23 14:02:29 +0330

    add Quill editor

--- components/QuestionDetail.js
+++ components/QuestionDetail.js
@@ -5,7 +5,9 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import axios from "axios";
 import { useState } from "react";
-
+import dynamic from "next/dynamic";
+const ReactQuill = dynamic(() => import("react-quill"), { ssr: false });
+import "react-quill/dist/quill.snow.css";
 
 const validationSchema = Yup.object({
   book: Yup.string()
@@ -16,22 +18,32 @@ const validationSchema = Yup.object({
     .min(4, "حداقل 4 حرف "),
 });
 
-const QuestionDetail = () => {
-  let initialValues  = { book: "", lesson: "", season: "", page: "" };
-  const [error , setError] = useState(" ") ; 
+const  modules  = {
+  toolbar: [
+      [{ header: [1, 2, 3, 4, 5, 6, false] }],
+      ["bold", "italic", "underline"],
+      [{ color: [] }, { background: [] }],
+      [{ script:  "sub" }, { script:  "super" }],
+      ["blockquote", "code-block"],
+      [{ list:  "ordered" }, { list:  "bullet" }],
+      [{ indent:  "-1" }, { indent:  "+1" }, { align: [] }],
+      [ "image", "video"],
+      ["clean"],
+  ],
+};
 
+const QuestionDetail = () => {
+  let initialValues = { book: "", lesson: "", season: "", page: "" };
+  const [error, setError] = useState(" ");
 
   const submitHanlder = async (values) => {
-   try {
-     await  axios
-     .post("http://localhost:3001/question", { ...values })
-     toast.success("مشخصات سوال ثبت گردید ")
-   } catch (error){
-     setError("b")
-
-   }
-  
-  }
+    try {
+      await axios.post("http://localhost:3001/question", { ...values });
+      toast.success("مشخصات سوال ثبت گردید ");
+    } catch (error) {
+      setError("b");
+    }
+  };
 
   const formik = useFormik({
     initialValues,
@@ -40,86 +52,92 @@ const QuestionDetail = () => {
     onSubmit: submitHanlder,
   });
 
-if(error === "a") return <p></p>
-if(error === "b" )return <p style={{position:"absolute" , left:"48%", top : "53%"}}></p>
+  if (error === "a") return <p></p>;
+  if (error === "b")
+    return <p style={{ position: "absolute", left: "48%", top: "53%" }}></p>;
   return (
-    <div className={styles.description}>
-      <div className={styles.title}>
-        <i class="bi bi-question-circle"></i>
-        <h6 className={styles.titleText}> </h6>
-      </div>
-      <form className={styles.form} noValidate onSubmit={formik.handleSubmit}>
-        <div className={styles.textField}>
-          <input
-            value={formik.values.lesson}
-            required
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            type="search"
-            name="lesson"
-          />
-          {formik.errors.lesson && formik.touched.lesson && (
-            <div className={styles.ereor}>{formik.errors.lesson}</div>
-          )}
-          <label>عنوان درس</label>
+    <>
+      <div className={styles.description}>
+        <div className={styles.title}>
+          <i class="bi bi-question-circle"></i>
+          <h6 className={styles.titleText}> </h6>
         </div>
-        <div className={styles.textField}>
-          <input
-            value={formik.values.book}
-            required
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            type="search"
-            name="book"
-          />
-          {formik.errors.book && formik.touched.book && (
-            <div className={styles.ereor}>{formik.errors.book}</div>
-          )}
-
-          <label>عنوان منبع </label>
-        </div>
-        <div className={styles.textField}>
-          <input
-            type="number"
-            name="season"
-            pattern="[0-9\-]+"
-            min="1"
-            required
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.season}
-          />
-          <label> شماره فصل </label>
-        </div>
-
-        {
+        <form className={styles.form} noValidate onSubmit={formik.handleSubmit}>
           <div className={styles.textField}>
             <input
-              type="text"
-              name="page"
-              min="1"
+              value={formik.values.lesson}
               required
-              
               onChange={formik.handleChange}
               onBlur={formik.handleBlur}
-              value={formik.values.page}
+              type="search"
+              name="lesson"
             />
-            <label> شماره صفحه </label>
+            {formik.errors.lesson && formik.touched.lesson && (
+              <div className={styles.ereor}>{formik.errors.lesson}</div>
+            )}
+            <label>عنوان درس</label>
           </div>
-        }
+          <div className={styles.textField}>
+            <input
+              value={formik.values.book}
+              required
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              type="search"
+              name="book"
+            />
+            {formik.errors.book && formik.touched.book && (
+              <div className={styles.ereor}>{formik.errors.book}</div>
+            )}
 
-        <div className={styles.btnContainer}>
-          <button
-            type="submit"
-            className={styles.submitButton}
-            disabled={!formik.isValid}
-          >
-            ثبت
-          </button>
-        </div>
-        <ToastContainer rtl={true} position="top-center" />
-      </form>
-    </div>
+            <label>عنوان منبع </label>
+          </div>
+          <div className={styles.textField}>
+            <input
+              type="number"
+              name="season"
+              pattern="[0-9\-]+"
+              min="1"
+              required
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.season}
+            />
+            <label> شماره فصل </label>
+          </div>
+
+          {
+            <div className={styles.textField}>
+              <input
+                type="text"
+                name="page"
+                min="1"
+                required
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.page}
+              />
+              <label> شماره صفحه </label>
+            </div>
+          }
+
+          <div className={styles.btnContainer}>
+            <button
+              type="submit"
+              className={styles.submitButton}
+              disabled={!formik.isValid}
+            >
+              ثبت
+            </button>
+          </div>
+          <ToastContainer rtl={true} position="top-center" />
+        </form>
+   <div className={styles.edtitorContainer}>
+   <ReactQuill className={styles.editor} modules={modules} />
+   </div>
+      </div>
+
+    </>
   );
 };
 
--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,8 @@ import '../styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-icons/font/bootstrap-icons.css'
 
+
+
 function MyApp({ Component, pageProps }) {
   return (
     <Provider
